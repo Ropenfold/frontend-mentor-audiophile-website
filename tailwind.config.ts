506cf64d2610abb11441bb3795da6a088ac73968
stuart.roper <stuart.roper@bravissimo.com>
2024-11-12 10:34:04 +0000

    add colours to tailwind.config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,8 +9,8 @@ const config: Config = {
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        black: '#000000',
+        white: '#FFFFFF'
       },
     },
   },
