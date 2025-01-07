3de4fe21202bc126681e6167ed8368aeb0cbad88
stuart.roper <stuart.roper@bravissimo.com>
2025-01-07 12:49:25 +0000

    create homepage layout with GraphQL

--- app/page.tsx
+++ app/page.tsx
@@ -1,101 +1,91 @@
-import Image from "next/image";
+import { fetchContentful } from '@/lib/contentful';
+import { PageContentCollection, PageContentItem, PageCollectionResponse } from '@/redux/types/navigationTypes';
+import HeroSectionComponent from './components/heroSectionComponent/heroSectionComponent';
 
-export default function Home() {
+const GET_HOMEPAGE_DATA = `query {
+  pageCollection(where: { pageName: "Homepage" }, limit: 1) {
+    items {
+      pageName
+      pageContentCollection {
+        items {
+          __typename
+          ... on HeroSection {
+            heroTitle
+            isNewProduct
+            heroImageCollection {
+              items {
+                url 
+                title
+                description
+              }
+            }
+            heroText
+            button {
+							... on Button {
+                buttonText
+                buttonTitle
+                textColour
+                linkUrl
+                colour
+                hoverColour
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+}`
+
+const createLayoutComponents = (pageContentCollection: PageContentCollection) => {
+  return pageContentCollection.items.map((item: PageContentItem, index: number) => {
+    console.log('item.__typename', item.__typename);
+    switch (item.__typename) {
+      case 'HeroSection':
+        console.log('in HeroSectionComponent');
+        return <HeroSectionComponent key={`hero-${index}`} {...item} />;
+      default:
+        return null; // In case the __typename doesn't match 'HeroSection'
+    }
+  });
+};
+
+export default async function Home() {
+
+const data = await fetchContentful<PageCollectionResponse>(GET_HOMEPAGE_DATA)
+const {
+  pageCollection: {
+    items: [
+      {
+        pageContentCollection,
+      },
+    ],
+  },
+} = data;
+
+console.log('pageContentCollection', pageContentCollection);
+
+  const layout = createLayoutComponents(pageContentCollection)
+
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+    <div>
+      {layout}
+    <div>Homepage</div>
+    <div>Homepage</div>
+    <div>Homepage</div>
+    <div>Homepage</div>
+    <div>Homepage</div>
+    <div>Homepage</div>
+    <div>Homepage</div>
+    <div>Homepage</div>
+    <div>Homepage</div>
+    <div>Homepage</div>
+    <div>Homepage</div>
+    <div>Homepage</div>
+    <div>Homepage</div>
+    <div>Homepage</div>
+    <div>Homepage</div>
+    <div>Homepage</div>
     </div>
   );
 }
